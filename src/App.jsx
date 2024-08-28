818e96a0f01f99ebfd0c6849e18ba911cc67e21b
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-28 20:46:07 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className="bg-primary w-full overflow-hidden">App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 export default App;
