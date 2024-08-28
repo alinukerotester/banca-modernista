2963be575dc39b5e90fc86a16ed8280a9cd82144
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-28 16:13:48 +0300

    gata putem continua

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,7 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
 
-// Configurare pentru ESLint
 export default [
   {
     ignores: ['dist'],
@@ -29,7 +27,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier, // Înlocuiește array-ul cu un obiect
+      prettier,
     },
     rules: {
       ...js.configs.recommended.rules,
