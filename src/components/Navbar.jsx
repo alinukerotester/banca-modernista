a95856940bc0ec2166cba84ebb77315d21389add
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-29 21:21:07 +0300

    gata pentru azi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import { logo } from '../assets';
+import { useState } from 'react';
+import { logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
@@ -15,6 +17,18 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
+        ></div>
+      </div>
     </nav>
   );
 };
