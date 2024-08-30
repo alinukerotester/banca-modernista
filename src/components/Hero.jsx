04d30d19b7af060643cb1ae1482bb828c401df61
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-08-30 18:42:18 +0300

    up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { discount } from '../assets';
 import styles from '../style';
+import GetStarted from './GetStarted';
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -14,11 +15,14 @@ const Hero = () => (
         </p>
       </div>
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] ">
           The Next <br className="sm:block hidden" />{' '}
           <span className="text-gradient">Generation</span>
           {'  '} Payment Method.
         </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+          <GetStarted />
+        </div>
       </div>
     </div>
   </section>
