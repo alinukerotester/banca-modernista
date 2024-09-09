d08d8edcd197d17a9f77c1d244d0fa156a4ef9e8
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-09-09 22:13:19 +0300

    up

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -4,9 +4,9 @@ import CardDeal from '../CardDeal/CardDeal';
 import Business from '../Business/Business';
 import Clients from '../Clients/Clients';
 import CTA from '../CTA/CTA';
-import Stats from '../stat/Stats';
+import Stats from '../Stats/Stats';
 import Footer from '../Footer/Footer';
-import Testimonials from '../testimonii/Testimonials';
+import Testimonials from '../Testimonials/Testimonials';
 import Hero from '../Hero/Hero';
 
 export {
