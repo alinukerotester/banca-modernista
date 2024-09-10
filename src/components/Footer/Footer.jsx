e6ab223a971b80a82219986c889d519bb59a999b
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2024-09-10 15:53:54 +0300

    poza footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,18 @@
-const Footer = () => {
-  return <div></div>;
-};
+import { logo } from '../../assets';
+import styles from '../../style';
+
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div>
+        <img
+          src={logo}
+          alt="hoobank"
+          className="w-[266px] h-[72px] object-contain"
+        />
+      </div>
+    </div>
+  </section>
+);
 
 export default Footer;
